374221246904dc6c7e3106c58a25f9510240e53f
Essi Monneus <essimonneus@gmail.com>
2021-08-08 10:37:10 -0400

    update role based access

--- controllers/user.js
+++ controllers/user.js
@@ -96,18 +96,3 @@ export const deleteUser = async (req, res, next) => {
   }
 }
 
-export const grantAccess = function (action, resource) {
-  return async (req, res, next) => {
-    try {
-      const permission = roles.can(req.user.role)[action](resource);
-      if (!permission.granted) {
-        return res.status(401).json({
-          error: "You don't have enough permission to perform this action"
-        });
-      }
-      next()
-    } catch (error) {
-      next(error)
-    }
-  }
-}
