b9a6a5e930f1262707f4a3bff0a04fe707e10a2f
Essi Monneus <essimonneus@gmail.com>
2021-08-08 10:54:14 -0400

    make it better

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
+app.get('/',(req, res) => {
+  res.send('Welcome to Recipe API')
+})
 
 
-const CONNECTION_URL = 'mongodb+srv://dbEssi:dbessi123@cluster0.gn3qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
